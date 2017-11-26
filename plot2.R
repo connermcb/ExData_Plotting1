2ce98a928249897b15b8f2390a217512ad81084b
connermcb <connermcb@yahoo.com>
2017-11-26 16:37:49 -0600

    add as.numeric formatting, change name of data file

--- plot2.R
+++ plot2.R
@@ -1,11 +1,14 @@
 ## Data Science Specialization - Exploratory Data Analysis - Week 1
 ## Plot 2: Global Active Power Time Series Line Plot
 
+# get data getter
+source(data_formatter.R)
+
 # open png plot device
 png("plot2.png", width = 480, height = 480, units = "px")
 
 # global active power time series line plot
-with(pwr2, plot(date_time, Global_active_power,
+with(pwr, plot(date_time, as.numeric(Global_active_power),
                 type="l",
                 ylab = "Global Active Power (kilowatts)",
                 xlab = NA)

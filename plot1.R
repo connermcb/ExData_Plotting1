a61b590cd5a31b964abd1ef94157ee3566d34959
connermcb <connermcb@yahoo.com>
2017-11-26 16:31:14 -0600

    add source(data_formatter.R) call and as.numeric format on plot data

--- plot1.R
+++ plot1.R
@@ -1,15 +1,17 @@
 ## Data Science Specialization - Exploratory Data Analysis - Week 1
 ## Plot 1: Global Active Power Histogram
 
+# get data getter
+source(data_formatter.R)
+
 # open png plot device
 png("plot1.png", width = 480, height = 480, units = "px")
 
 # plot
-hist(pwr2$Global_active_power/1000, breaks=16,
+hist(as.numeric(pwr$Global_active_power),
      col="red",
      main="Global Active Power",
-     xlab="Global Active Power (kilowatts)",
-     ylab="Frequency")
+     xlab="Global Active Power (kilowatts)")
 
 # close device
 dev.off()

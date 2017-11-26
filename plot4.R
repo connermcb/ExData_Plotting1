cb3de0e69fd20eee3c8a715896d50193b470ec5a
connermcb <connermcb@yahoo.com>
2017-11-26 16:40:59 -0600

    as.numeric format for plot variables, change data name

--- plot4.R
+++ plot4.R
@@ -9,7 +9,7 @@ par(mfcol=c(2,2))
 
 ## Plot 1 ----------------------------------------------------------
 # global active power time series line plot
-with(pwr2, plot(date_time, Global_active_power,
+with(pwr, plot(date_time, as.numeric(Global_active_power),
                 type="l",
                 ylab = "Global Active Power",
                 xlab = NA)
@@ -17,22 +17,26 @@ with(pwr2, plot(date_time, Global_active_power,
 
 ## Plot 2 ------------------------------------------------------------
 # plot panel and `Sub_metering_1` line
-with(pwr2, plot(date_time, Sub_metering_1,
+with(pwr, plot(date_time, as.numeric(Sub_metering_1),
                 type="l",
                 ylab = "Energy sub metering",
                 xlab = NA)
 )
 
 # add `Sub_metering_2` line
-with(pwr2, lines(date_time, Sub_metering_2, col="red"))
+with(pwr, lines(date_time, as.numeric(Sub_metering_2), col="red"))
 
 
 # add `Sub_metering_3` line
-with(pwr2, lines(date_time, Sub_metering_3, col="blue"))
+with(pwr, lines(date_time, as.numeric(Sub_metering_3), col="blue"))
+
+# add legend
+legend("topright", lty=c(1,1,1), lwd=c(1.5, 1.5), col=c("black", "red", "blue"),
+       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
 ## Plot 3 ----------------------------------------------------------
 # global voltage time series line plot
-with(pwr2, plot(date_time, Voltage,
+with(pwr2, plot(date_time, as.numeric(Voltage),
                 type="l",
                 ylab = "Voltage",
                 xlab = "datetime")
@@ -40,7 +44,7 @@ with(pwr2, plot(date_time, Voltage,
 
 ## Plot 4 ----------------------------------------------------------
 # global reactive power time series line plot
-with(pwr2, plot(date_time, Global_reactive_power,
+with(pwr2, plot(date_time, as.numeric(Global_reactive_power),
                 type="l",
                 ylab = "Global_reactive_power",
                 xlab = "datetime")
@@ -48,3 +52,4 @@ with(pwr2, plot(date_time, Global_reactive_power,
 
 # close device
 dev.off()
+

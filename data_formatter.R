c03e65ff2aee0a7a2ab34a8a1d862cdad4254016
connermcb <connermcb@yahoo.com>
2017-11-26 16:32:12 -0600

    correct dates on subset() call, remove calls to plotting files

--- data_formatter.R
+++ data_formatter.R
@@ -1,37 +1,23 @@
 ## Data Science Specialization - Exploratory Data Analysis - Week 1
 ## Retrieve and Format Data
 
-# download and unzip data
-temp <- tempfile()
-download.file(
-  "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip",
-  temp)
-con <- unz(temp, "household_power_consumption.txt")
-data <- read.csv(con, sep=";")
-unlink(temp)
+# load libraries
+library(readr)
 
-closeAllConnections()
+# read data
+pwr <- read_csv2("household_power_consumption.txt", na=c("?", ""))
 
 ## format data and subset data
 # date to character class
-data[,"Date"] <- as.character(data$Date)
+pwr[,"Date"] <- as.character(pwr$Date)
 
 # subset on assignment dates
-data <- subset(data, Date %in% c("2/1/2007", "2/2/2007"))
+pwr <- pwr[pwr$Date=="1/2/2007" | pwr$Date=="2/2/2007",]
 
-# change class of numeric data
-for( i in 3:9){
-  data[, i] <- as.numeric(data[, i])
-}
-
-# new variable combining date and time
-data["date_time"] <- paste(as.character(data$Date), as.character(data$Time))
+# new variable combining date and timepwr
+pwr["date_time"] <- paste(as.character(pwr$Date), as.character(pwr$Time))
 
 # change class of date_time to POSIXt
-data[["date_time"]] <- strptime(data$date_time, format = "%d/%m/%Y %H:%M:%S")
+pwr[["date_time"]] <- strptime(pwr$date_time, format = "%d/%m/%Y %H:%M:%S")
 
-## Calls to plotting functions
-source("plot1.R")
-source("plot2.R")
-source("plot3.R")
-source("plot4.R")
+
